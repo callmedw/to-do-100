d9fb2a66c12fd42fdfffda8a8251a021d2dec503
Dana Weiss and Susha Dore <susha@>
2017-06-07 13:17:26 -0700

    add complete pipe to categorize tasks as complete or incompelete

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -4,18 +4,35 @@ import { Task }       from './task.model';
 @Component({
   selector: 'task-list',
   template: `
-  <ul>
-    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
-  </ul>
+    <select (change)="onChange($event.target.value)">
+      <option value="allTasks">All Tasks</option>
+      <option value="completedTasks">Completed Tasks</option>
+      <option value="incompleteTasks">Incomplete Tasks</option>
+    </select>
+    <ul>
+      <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}} {{currentTask.priority}}
+      <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)">
+      <input *ngIf="currentTask.done === false" type="checkbox" checked (click)="toggleDone(currentTask, true)">
+      <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
+    </ul>
   `
 })
 
 export class TaskListComponent {
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
+  filterByCompleteness: string = "incompleteTasks";
 
   editButtonHasBeenClicked(taskToEdit: Task) {
     this.clickSender.emit(taskToEdit);
+  }
+
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
+  }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
   }
 
   isDone(clickedTask: Task) {
